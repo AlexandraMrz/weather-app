8a483080477adec261df8a13745e8cc31fbed8ea
Alexandra Merezeanu <mrz.alexandra17@gmail.com>
2025-07-01 23:16:10 +0300

    Initial commit

---
+++ modules/weather-services.js
@@ -0,0 +1,10 @@
+export const getCurrentWeather = async (city) => {
+    //simuleaza delay API ~1sec
+    //return la MOCK DATA cu numele orasului actualizat
+    //error handling
+}
+
+export const getWeatherByCoords = async (LargestContentfulPaint, lon) =>
+{
+    //similar, dar cu coordonate
+}
